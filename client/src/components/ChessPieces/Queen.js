b2c440d916faedadb3bee0d50c11478ee2eb5a2b
Anthony Jung <junganthonyy@gmail.com>
2020-09-28 09:25:35 -0700

    Changing Name Of Test

--- client/src/components/ChessPieces/Queen.js
+++ client/src/components/ChessPieces/Queen.js
@@ -52,45 +52,6 @@ export default class Queen extends Piece {
       path.push([src.row, src.col]);
     }
 
-
-
-    // // check if on same row or column
-    // if (src.row === dest.row) {
-    //   // then we're moving horizontally on the same row
-    //   let diff = Math.abs(dest.col - src.col);
-    //   let interval = ((dest.col - src.col) > 0 ? 1 : -1);
-    //   let curr = src.col;
-
-    //   // diff - 1 because we don't want to
-    //   // include the start and the end
-    //   for (let i = 0; i < diff - 1 && curr > 0; i += 1) {
-    //     curr += interval;
-    //     path.push([src.row, curr])
-    //   }
-    // } else if (src.col === dest.col) {
-    //   // then we're moving horizontally on the same row
-    //   let diff = Math.abs(dest.row - src.row);
-    //   let interval = ((dest.row - src.row) > 0 ? 1 : -1);
-    //   let curr = src.col;
-
-    //   // diff - 1 because we don't want to
-    //   // include the start and the end
-    //   for (let i = 0; i < diff - 1 && curr > 0; i += 1) {
-    //     curr += interval;
-    //     path.push([curr, src.col])
-    //   }
-    // } else {
-    //   // now we're dealing with diagonals
-    //   let rowInterval = (dest.row - src.row) / (Math.abs(dest.row - src.row));
-    //   let colInterval = (dest.col - src.col) / (Math.abs(dest.col - src.col));
-    //   let diff = Math.abs(dest.col - src.col);
-      
-    //   for (let i = 0; i < diff - 1; i++) {
-    //     src.row += rowInterval;
-    //     src.col += colInterval;
-    //     path.push([src.row, src.col]);
-    //   }
-    // }
     return path;
   }
 }
